2d502583c1ad878929d2a782dff6399a4af0b43d
Lesley Iazzag <26lci1@cow-i23-nuc34.cs.williams.edu>
2025-11-15 19:18:24 -0500

    deletes

--- test3.cc
+++
@@ -1,8 +0,0 @@
-// verify that newly extended pages are zero-initialized
-#include "vm_app.h"
-#include <iostream>
-
-int main() {
-    char *p = (char *) vm_extend();
-    vm_syslog(p, 4); // should print 4 null chars 
-}
